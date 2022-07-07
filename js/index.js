ee200641f08454f007aedd5d2a2bc74ba9ed8c2b
Alisa Zhurova <alisafedulova2013@gmail.com>
2022-07-07 10:43:55 +0300

    add contacts page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 const windowInnerWidth = document.documentElement.clientWidth;
-
+//responsible width menu
 jQuery(window).bind('load resize', function(){
   var nav = jQuery('.menu__mobile ul'), 
   w = window.innerWidth;
@@ -9,3 +9,15 @@ jQuery(window).bind('load resize', function(){
       width: w
       });
 });
+
+//update color for scroll header
+var scrolled;
+window.onscroll = function() {
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrolled > 120){
+      jQuery("header").css({"background": "linear-gradient(180deg, rgb(45 46 47) 0%, rgb(0 0 0) 100%"})
+    }
+    else{
+      jQuery("header").css({"background": "linear-gradient(180deg, rgba(45, 46, 47, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%"})
+    }
+}
